d1219edbf237ff3799c7d661f251cdd8ba61b0f5
Toby Muir-Little <toby.muirlittle@simplybusiness.co.uk>
2017-06-07 14:22:16 +0100

    add journey routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  match "/journey/:name/(:step/)(:journey_id)" => "application#handle", via: [:get, :post], as: :journey
+
 end
