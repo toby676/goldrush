452c4d3ffebff4d14f699ac046a3bae18350ece2
Toby Muir-Little <toby.muirlittle@simplybusiness.co.uk>
2017-06-08 11:54:16 +0100

    add welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+
+  root 'welcome#index'
+
+  get 'welcome/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
   match "/journey/:name/(:step/)(:journey_id)" => "application#handle", via: [:get, :post], as: :journey
 
 end
