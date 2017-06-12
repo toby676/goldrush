b845d27bb26c3ab8c0543f17856a176c905d459a
Toby Muir-Little <toby.muirlittle@simplybusiness.co.uk>
2017-06-12 10:03:19 +0100

    allow text to be saved

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < Journey::Rails::JourneyController
   protect_from_forgery with: :exception
+
+  def context
+    OpenStruct.new(params: params.permit(:journey_token, :text))
+  end
+
 end
