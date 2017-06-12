c762a0b13c7bb2c147cbb89b285a8d976988ff66
Toby Muir-Little <toby.muirlittle@simplybusiness.co.uk>
2017-06-12 10:06:10 +0100

    allow text to be accessed by the hello page service and show on the hellow view

--- app/journey/goldrush/hello_page_service.rb
+++ app/journey/goldrush/hello_page_service.rb
@@ -2,13 +2,22 @@ module Goldrush
   class HelloPageService < Journey::StepService
 
     def enter
-      {journey_state: journey_state}
+      {
+        journey_state: journey_state,
+        text: text
+      }
     end
 
     def process
       # TODO validate and store the submitted data
       # return an instance of SuccessResult or FailureResult
       Journey::SuccessResult.new
+    end
+
+    private
+
+    def text
+      journey_data[:text]
     end
 
   end
